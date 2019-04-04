035b103f7ab979a147396a2245d60df68149377d
thfrwn <11335318+rfht@users.noreply.github.com>
2019-04-04 08:39:55 -0700

    add initial scaffolding for python packaging

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+# drafted from https://python-packaging.readthedocs.io/en/latest/minimal.html
+# url abcdqc.org or github repo?
+# author? email?
+
+setup(name='abcdqc_batchserver',
+    version='0.1',
+    description='Batch server for the abcdqc project',
+    url='http://www.abcdqc.org',
+    author='ABCDQC Team',
+    email='11335318+rfht@users.noreply.github.com',
+    license='CC0',
+    packages=['abcdqc_batchserver'],
+    zip_safe=False)
